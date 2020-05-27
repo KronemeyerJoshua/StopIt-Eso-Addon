527ce17bd6e3099a05e15a05a2d9a267d6d043db
KronemeyerJoshua <jkroneme@asu.edu>
2020-05-27 16:34:53 -0700

    Rewrote removal of FRAME_PLAYER_FRAGMENT from FRAGMENT_GROUP and TARGETED_SCENES. Added support for Antiquities

--- StopIt!.lua
+++ StopIt!.lua
@@ -7,39 +7,57 @@ StopIt = {
 function StopIt.OnAddOnLoaded(_,addOnName)
 	if (addOnName ~= StopIt.name) then return end
 
+    -- These are the scenes we know still work correctly when FRAME_PLAYER_FRAGMENT is removed
+	TARGETED_SCENES = {
+		'inventory',
+		'stats',
+		'skills',
+		'questJournal',
+		'achievements',
+		'leaderboards',
+		'loreLibrary',
+		'collectionsBook',
+		'dlcBook',
+		'friendsList',
+		'ignoreList',
+		'groupMenuKeyboard',
+		'guildHome',
+		'guildRoster',
+		'guildRanks',
+		'guildHeraldry',
+		'guildHistory',
+		'guildCreate',
+		'campaignOverview',
+		'campaignBrowser',
+		'mailInbox',
+		'guildBrowserKeyboard',
+		'mailSend',
+		'notifications',
+		'gameMenuInGame',
+		'helpCustomerSupport',
+		'helpEmotes',
+		'helpTutorials',
+		'championPerks',
+		'antiquityJournalKeyboard'
+	}
+
+    -- Removing FRAME_PLAYER_FRAGMENT from targeted scenes
+	for k, v in pairs(TARGETED_SCENES) do
+		SCENE_MANAGER:GetScene(v):RemoveFragment(FRAME_PLAYER_FRAGMENT)
+	end
 	-- Using a for loop on SCENE_MANAGER.scenes causes a lot of bugs... Targeting specific scenes instead
-    SCENE_MANAGER:GetScene('inventory'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('stats'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('skills'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('questJournal'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('achievements'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('leaderboards'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('loreLibrary'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('collectionsBook'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('dlcBook'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('friendsList'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('ignoreList'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('groupMenuKeyboard'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('guildHome'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('guildRoster'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('guildRanks'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('guildHeraldry'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('guildHistory'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('guildCreate'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('campaignOverview'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('campaignBrowser'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-    SCENE_MANAGER:GetScene('mailInbox'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-    SCENE_MANAGER:GetScene('guildBrowserKeyboard'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('mailSend'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('notifications'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('gameMenuInGame'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('helpCustomerSupport'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('helpEmotes'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('helpTutorials'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	SCENE_MANAGER:GetScene('championPerks'):RemoveFragment(FRAME_PLAYER_FRAGMENT)
 	SCENE_MANAGER:GetScene('championPerks'):RemoveFragment(END_IN_WORLD_INTERACTIONS_FRAGMENT)
 	SCENE_MANAGER:GetScene('cadwellsAlmanac'):RemoveFragmentGroup(FRAGMENT_GROUP.FRAME_TARGET_STANDARD_RIGHT_PANEL)
 
+	-- OVERRIDING GLOBAL VARS TO REMOVE FRAME_PLAYER_FRAGMENT FOR THIRD PARTY ADDONS
+	for k, v in pairs (FRAGMENT_GROUP) do
+    	if FRAGMENT_GROUP[k].FRAME_PLAYER_FRAGMENT ~= nil then
+    		FRAGMENT_GROUP[k].remove(FRAME_PLAYER_FRAGMENT)
+    	end
+	end
+
+	--Unregister Loaded Callback
+    EVENT_MANAGER:UnregisterForEvent(StopIt.name, EVENT_ADD_ON_LOADED)
 end
 
 -- GARKINS NonstopHarvest
@@ -56,194 +74,3 @@ EVENT_MANAGER:UnregisterForEvent("IngameSceneManager", EVENT_NEW_MOVEMENT_IN_UI_
 EVENT_MANAGER:RegisterForEvent(StopIt.name, EVENT_ADD_ON_LOADED, StopIt.OnAddOnLoaded)
 -- Disable mount from setting UI mode to false
 EVENT_MANAGER:UnregisterForEvent("IngameSceneManager", EVENT_MOUNTED_STATE_CHANGED)
-
--- OVERRIDING GLOBAL VARS TO REMOVE FRAME_PLAYER_FRAGMENT FOR THIRD PARTY ADDONS
--- If this addon suddenly stops working, go to the below url. Copy and paste, remove all instances of FRAME_PLAYER_FRAGMENT
--- https://esodata.uesp.net/current/src/ingame/scenes/ingamefragmentgroups.lua.html
-FRAGMENT_GROUP =
-{
-    MOUSE_DRIVEN_UI_WINDOW =
-    {
-        MOUSE_UI_MODE_FRAGMENT,
-        KEYBIND_STRIP_FADE_FRAGMENT,
-        KEYBIND_STRIP_MUNGE_BACKDROP_FRAGMENT,
-        UI_SHORTCUTS_ACTION_LAYER_FRAGMENT,
-        CLEAR_CURSOR_FRAGMENT,
-        UI_COMBAT_OVERLAY_FRAGMENT,
-        END_IN_WORLD_INTERACTIONS_FRAGMENT,
-    },
-    MOUSE_DRIVEN_UI_WINDOW_NO_COMBAT_OVERLAY =
-    {
-        MOUSE_UI_MODE_FRAGMENT,
-        KEYBIND_STRIP_FADE_FRAGMENT,
-        UI_SHORTCUTS_ACTION_LAYER_FRAGMENT,
-        CLEAR_CURSOR_FRAGMENT,
-        END_IN_WORLD_INTERACTIONS_FRAGMENT,
-    },
-    MOUSE_DRIVEN_UI_WINDOW_NO_KEYBIND_STRIP =
-    {
-        MOUSE_UI_MODE_FRAGMENT,
-        UI_SHORTCUTS_ACTION_LAYER_FRAGMENT,
-        CLEAR_CURSOR_FRAGMENT,
-        UI_COMBAT_OVERLAY_FRAGMENT,
-        END_IN_WORLD_INTERACTIONS_FRAGMENT,
-    },
-    GAMEPAD_DRIVEN_UI_WINDOW =
-    {
-        MOUSE_UI_MODE_FRAGMENT, --still need this as it blocks clicks from becoming attacks
-        GAMEPAD_UI_MODE_FRAGMENT,
-        KEYBIND_STRIP_GAMEPAD_FRAGMENT,
-        KEYBIND_STRIP_GAMEPAD_BACKDROP_FRAGMENT,
-        UI_SHORTCUTS_ACTION_LAYER_FRAGMENT,
-        CLEAR_CURSOR_FRAGMENT,
-        UI_COMBAT_OVERLAY_FRAGMENT,
-        END_IN_WORLD_INTERACTIONS_FRAGMENT,
-        HIDE_MOUSE_FRAGMENT,
-    },
-    GAMEPAD_DRIVEN_UI_NO_KEYBIND_BACKGROUND_WINDOW =
-    {
-        MOUSE_UI_MODE_FRAGMENT, --still need this as it blocks clicks from becoming attacks
-        GAMEPAD_UI_MODE_FRAGMENT,
-        KEYBIND_STRIP_GAMEPAD_FRAGMENT,
-        UI_SHORTCUTS_ACTION_LAYER_FRAGMENT,
-        CLEAR_CURSOR_FRAGMENT,
-        UI_COMBAT_OVERLAY_FRAGMENT,
-        END_IN_WORLD_INTERACTIONS_FRAGMENT,
-        HIDE_MOUSE_FRAGMENT,
-    },
-    FRAME_TARGET_CENTERED =
-    {
-        FRAME_TARGET_CENTERED_FRAGMENT,
-        FRAME_TARGET_BLUR_CENTERED_FRAGMENT,
-    },
-    FRAME_TARGET_CENTERED_NO_BLUR =
-    {
-        FRAME_TARGET_CENTERED_FRAGMENT,
-    },
-    FRAME_TARGET_STANDARD_RIGHT_PANEL =
-    {
-        FRAME_TARGET_STANDARD_RIGHT_PANEL_FRAGMENT,
-        FRAME_TARGET_BLUR_STANDARD_RIGHT_PANEL_FRAGMENT,
-    },
-    FRAME_TARGET_STANDARD_RIGHT_PANEL_MEDIUM_LEFT_PANEL =
-    {
-        FRAME_TARGET_STANDARD_RIGHT_PANEL_MEDIUM_LEFT_PANEL_FRAGMENT,
-        FRAME_TARGET_BLUR_STANDARD_RIGHT_PANEL_MEDIUM_LEFT_PANEL_FRAGMENT,
-    },
-    FRAME_TARGET_FURNITURE_BROWSER_NO_BLUR =
-    {
-        FRAME_TARGET_FURNITURE_BROWSER_FRAGMENT,
-    },
-    FRAME_TARGET_CENTERED_UNIFORM_BLUR =
-    {
-        FRAME_TARGET_CENTERED_FRAGMENT,
-        UNIFORM_BLUR_FRAGMENT,
-    },
-    FRAME_TARGET_OPTIONS =
-    {
-        FRAME_TARGET_OPTIONS_FRAGMENT,
-    },
-    FRAME_TARGET_GAMEPAD =
-    {
-        FRAME_TARGET_GAMEPAD_FRAGMENT,
-        FRAME_TARGET_BLUR_GAMEPAD_FRAGMENT,
-    },
-    FRAME_TARGET_GAMEPAD_OPTIONS =
-    {
-        FRAME_TARGET_GAMEPAD_OPTIONS_FRAGMENT,
-    },
-    FRAME_TARGET_GAMEPAD_LEFT =
-    {
-        FRAME_TARGET_LEFT_GAMEPAD_FRAGMENT,
-        FRAME_TARGET_LEFT_BLUR_GAMEPAD_FRAGMENT,
-    },
-    READ_ONLY_EQUIPPED_ITEMS =
-    {
-        THIN_LEFT_PANEL_BG_FRAGMENT,
-        READ_ONLY_CHARACTER_WINDOW_FRAGMENT,
-    },
-    PLAYER_PROGRESS_BAR_KEYBOARD_CURRENT =
-    {
-        PLAYER_PROGRESS_BAR_FRAGMENT,
-        PLAYER_PROGRESS_BAR_CURRENT_FRAGMENT,
-    },
-    PLAYER_PROGRESS_BAR_GAMEPAD =
-    {
-        PLAYER_PROGRESS_BAR_FRAGMENT,
-        PLAYER_PROGRESS_BAR_GAMEPAD_NAME_LOCATION_ANCHOR_FRAGMENT,
-    },
-    PLAYER_PROGRESS_BAR_GAMEPAD_CURRENT =
-    {
-        PLAYER_PROGRESS_BAR_FRAGMENT,
-        PLAYER_PROGRESS_BAR_CURRENT_FRAGMENT,
-        PLAYER_PROGRESS_BAR_GAMEPAD_NAME_LOCATION_ANCHOR_FRAGMENT,
-    },
-    FRAME_TARGET_GAMEPAD_RIGHT =
-    {
-        FRAME_TARGET_GAMEPAD_RIGHT_FRAGMENT,
-        FRAME_TARGET_BLUR_GAMEPAD_RIGHT_FRAGMENT,
-
-    },
-    FRAME_TARGET_GAMEPAD_RIGHT_FURTHER_AWAY =
-    {
-        FRAME_TARGET_GAMEPAD_RIGHT_FRAGMENT,
-        FRAME_TARGET_DISTANCE_GAMEPAD_FAR_FRAGMENT,
-        FRAME_TARGET_BLUR_GAMEPAD_RIGHT_FRAGMENT,
-    },
-    GAMEPAD_ACTIVITY_FINDER_DEPENDENCIES =
-    {
-        GAMEPAD_GROUP_ROLES_FRAGMENT,
-        GAMEPAD_NAV_QUADRANT_1_BACKGROUND_FRAGMENT,
-        MINIMIZE_CHAT_FRAGMENT,
-        GAMEPAD_MENU_SOUND_FRAGMENT,
-        GAMEPAD_ACTIVITY_QUEUE_DATA_FRAGMENT,
-        GAMEPAD_GENERIC_FOOTER_FRAGMENT,
-    },
-    HOUSING_EDITOR_HUD =
-    {
-        HOUSING_EDITOR_HUD_ACTION_LAYER_FRAGMENT,
-        UNIT_FRAMES_FRAGMENT,
-        COMPASS_FRAME_FRAGMENT
-    },
-    KEYBOARD_KEYBIND_STRIP_GROUP =
-    {
-        KEYBIND_STRIP_MUNGE_BACKDROP_FRAGMENT,
-        KEYBIND_STRIP_FADE_FRAGMENT,
-    },
-    GAMEPAD_KEYBIND_STRIP_GROUP =
-    {
-        KEYBIND_STRIP_GAMEPAD_BACKDROP_FRAGMENT,
-        KEYBIND_STRIP_GAMEPAD_FRAGMENT,
-    },
-    SUPRESS_COLLECTIBLES_GROUP =
-    {
-        SUPPRESS_COLLECTIBLE_NOTIFICATIONS_FRAGMENT,
-        SUPPRESS_COLLECTIBLE_ANNOUNCEMENTS_FRAGMENT,
-    },
-    SIEGE_BAR_GROUP =
-    {
-        SIEGE_ACTION_LAYER_FRAGMENT,
-        SIEGE_HUD_FRAGMENT,
-        PLAYER_PROGRESS_BAR_FRAGMENT,
-        UNIT_FRAMES_FRAGMENT,
-        PLAYER_ATTRIBUTE_BARS_FRAGMENT,
-        COMPASS_FRAME_FRAGMENT,
-        PERFORMANCE_METER_FRAGMENT,
-    },
-    BATTLEGROUND_SCOREBOARD_GROUP =
-    {
-        BATTLEGROUND_SCOREBOARD_FRAGMENT,
-        UNIT_FRAMES_FRAGMENT,
-        UI_COMBAT_OVERLAY_FRAGMENT,
-    },
-    BATTLEGROUND_MATCH_INFO_KEYBOARD_GROUP =
-    {
-        BATTLEGROUND_MATCH_INFO_KEYBOARD_FRAGMENT,
-        THIN_TALL_RIGHT_PANEL_BG_FRAGMENT,
-    },
-    BATTLEGROUND_MATCH_INFO_GAMEPAD_GROUP =
-    {
-        BATTLEGROUND_MATCH_INFO_GAMEPAD_FRAGMENT,
-        GAMEPAD_NAV_QUADRANT_4_BACKGROUND_FRAGMENT,
-    },
-}
